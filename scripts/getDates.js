6ca52e831c8cbcff8b83b27e229850edcf95875d
Borgesfacundo <borgesfacundo@gmail.com>
2024-03-05 23:01:21 -0300

    Week 01 starting page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,10 @@ const currentYear = currentDate.getFullYear();
 year = document.querySelector(".year");
 
 year.innerHTML = currentYear;
+
+// Adding last modification date
+lastModif = document.lastModified;
+const lastModElement = document.querySelector("#lastModified");
+const text = `Last modified: ${lastModif}`;
+
+lastModElement.textContent += text;
