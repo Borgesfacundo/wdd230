15bcecbd79cd20501cd7c14aa5a3dd5afad0ec12
Borgesfacundo <borgesfacundo@gmail.com>
2024-03-15 07:29:46 -0300

    Starting discover page

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -5,3 +5,14 @@ hamburgerElement.addEventListener("click", () => {
     navElement.classList.toggle("open");
     hamburgerElement.classList.toggle("open");
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const calendarEl = document.getElementById('calendar')
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+
+        initialView: 'dayGridMonth'
+    })
+
+    calendar.render()
+})
