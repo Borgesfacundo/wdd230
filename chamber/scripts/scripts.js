d9f829cc66cbff30293038eae246ae3a9bcf7b32
Borgesfacundo <borgesfacundo@gmail.com>
2024-03-15 14:24:24 -0300

    css grid week 3

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -5,14 +5,3 @@ hamburgerElement.addEventListener("click", () => {
     navElement.classList.toggle("open");
     hamburgerElement.classList.toggle("open");
 });
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const calendarEl = document.getElementById('calendar')
-    const calendar = new FullCalendar.Calendar(calendarEl, {
-
-        initialView: 'dayGridMonth'
-    })
-
-    calendar.render()
-})
