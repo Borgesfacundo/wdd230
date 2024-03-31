3dd85a808b53838dc07603700b88c552046cdedd
Borgesfacundo <bor22016@byui.edu>
2024-03-31 14:13:52 -0600

    asdasd

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,6 @@
 const baseURL = "https://github.com/Borgesfacundo/wdd230";
 const linksURL = "https://github.com/Borgesfacundo/wdd230/data/links.json";
+const weeksList = document.querySelector("#weeks");
 
 async function getLinks() {
     const response = await fetch(linksURL);
@@ -12,9 +13,16 @@ function displayLinks(weeks) {
     weeks.forEach((week) => {
         const list = document.createElement("li");
         const a = document.createElement("a");
+        const link = week.weeks.links[weeks].url;
+
+
         
-        list.textContent = week.weeks.
-    })
+        list.textContent = week.weeks.week;
+        a.setAttribute("href", link);
+        a.innerHTML = week.weeks.links[weeks].title;
+
+        list.appendChild(a);
+    });
 
 }
 
