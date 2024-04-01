521d2420f7341c0e2460196bad04e8d78cc0b0db
Borgesfacundo <bor22016@byui.edu>
2024-04-01 07:56:16 -0600

    All infor displayed

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,50 @@
+const linkURL = "https://borgesfacundo.github.io/wdd230/chamber/data/members.json";
+const article = document.querySelector("article");
+
+async function getMembers() {
+    const response = await fetch(linkURL);
+    const data = await response.json();
+    //console.log(data.companies);
+    displayCompanies(data.companies);
+
+}
+
+const displayCompanies = (members) => {
+    members.forEach((member) => {
+        //Create Elements
+        let card = document.createElement("section");
+        let name = document.createElement("h2");
+        let icon = document.createElement("img");
+        let info = document.createElement("h4");
+        let phone = document.createElement("h4");
+        let website = document.createElement("a");
+
+
+        //Set Title
+        name.textContent = member.name;
+
+        //set image
+        icon.setAttribute("src", member.image);
+        icon.setAttribute("alt", member.name);
+        icon.setAttribute("width", "300");
+        icon.setAttribute("width", "300");
+        icon.setAttribute("loading", "lazy");
+
+        //Set Information
+        info.innerHTML = `${member.slogan} - ${member.address}`;
+        phone.textContent = member.phoneNumber;
+        website.setAttribute("href", member.website);
+        website.innerHTML = member.website;
+
+        //Add all to the page
+        card.appendChild(name);
+        card.appendChild(icon);
+        card.appendChild(info);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        article.appendChild(card);
+    });
+}
+
+getMembers();
